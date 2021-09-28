8c19b037c459444b7cb77d7e8a4943d2ba5c7b3b
Marcosss2 <91058545+Marcosss2@users.noreply.github.com>
2021-09-28 11:33:04 +0200

    nuevo

---
+++ GestionVehiculo/src/com/ceep/test/PruebaGestion.java
@@ -0,0 +1,19 @@
+
+package com.ceep.test;
+
+import com.ceep.donain.Vehiculo;
+
+
+public class PruebaGestion {
+
+   
+    public static void main(String[] args) {
+       
+       Vehiculo vehiculo1 = new Vehiculo(); 
+
+       
+       
+       
+    }
+    
+}
